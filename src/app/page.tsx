d5fed84ba1a34e395c5d37bf63217a91c6ea8a39
prateek3065 <prateek.pixel@gmail.com>
2023-12-05 15:29:04 +0530

    changes for i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { useStore } from "@/store/store";
 import Image from "next/image";
 
-export default function Home() {
-  // const task = useStore((state) => state.tasks);
+import { Locale } from "../../i18n.config";
+import { getDictionary } from "./lib/dictionary";
+
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: any };
+}) {
+  const task = useStore((state) => state.tasks);
+  const { page } = await getDictionary(lang);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Root Page
