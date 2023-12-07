03750220b8dfcf617bff2d2c28d7cae632e61a17
prateek3065 <prateek.pixel@gmail.com>
2023-12-07 05:30:40 +0530

    debugged rendering and performance behavior

--- src/app/(lang)/about/page.tsx
+++ src/app/(lang)/about/page.tsx
@@ -1,12 +1,50 @@
 "use client";
-import React from "react";
-
+import React, { useState } from "react";
+const arrayLength = 22222;
+const longArray = Array.from({ length: arrayLength }, (_, index) => index);
+const randomColors = longArray.map((ele) => getRandomColor());
+const pixelSize = "4px";
+function getRandomColor() {
+  // Generate a random hexadecimal color code
+  const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  return color;
+}
 const Home: React.FC = () => {
+  const [state, setState] = useState(0);
   return (
     <>
       <h1>Welcome to About Section</h1>
+      <button
+        onClick={() => {
+          console.time("timer");
+          setState((p) => p + 1);
+        }}
+      >
+        Change State
+      </button>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {longArray.map((value) => {
+          return <Pixel key={value} value={value} />;
+        })}
+      </div>
     </>
   );
 };
 
 export default Home;
+
+const Pixel: React.FC<{ value: number }> = ({ value }) => {
+  if (value === arrayLength - 1) {
+    console.log("done");
+    console.timeEnd("timer");
+  }
+  return (
+    <div
+      style={{
+        width: pixelSize,
+        height: pixelSize,
+        backgroundColor: getRandomColor(),
+      }}
+    ></div>
+  );
+};
