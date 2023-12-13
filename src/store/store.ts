d2b738ba4c3eaa2e9e27d20c31a9dbb7de0c1967
prateek3065 <prateek.pixel@gmail.com>
2023-12-14 00:03:24 +0100

    interceptiion/middleware debugging

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,50 @@
 "use client";
 
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import counterReducer from "./feature/counter/counterSlice";
-import productsSlice from "./products/productsSlice";
+import productsSlice, { fetchProducts } from "./products/productsSlice";
+
+// const loggingMiddleware: Middleware = (storeAPI) => (next) => (action:any) => {
+//   if (action.type === fetchProducts.pending.type) {
+//     // This block will intercept when fetchProducts is called
+//     console.log('fetchProducts is pending');
+//     // You can perform actions here before the API call
+//   } else if (action.type === fetchProducts.fulfilled.type) {
+//     // This block will intercept when fetchProducts is completed successfully
+//     console.log('fetchProducts is fulfilled');
+//     // You can perform actions here after the API call is completed successfully
+//   } else if (action.type === fetchProducts.rejected.type) {
+//     // This block will intercept when fetchProducts is completed with an error
+//     console.log('fetchProducts is rejected');
+//     // You can perform actions here if the API call fails
+//   }
+//   return next(action);
+// };
+
+const loggingMiddleware: Middleware = (storeAPI) => (next) => (action: any) => {
+  if (action.type === fetchProducts.pending.type) {
+    // This block will intercept when fetchProducts is called
+    console.log("fetchProducts is pending");
+    // You can perform actions here before the API call
+
+    // Example condition to stop the action
+    const state: RootState = storeAPI.getState();
+    if (Math.ceil(Math.random() * 1000) % 2 === 0) {
+      console.log("Stopping fetchProducts action...");
+      return; // Prevent further processing of the action by not calling next(action)
+    }
+  } else if (action.type === fetchProducts.fulfilled.type) {
+    // This block will intercept when fetchProducts is completed successfully
+    console.log("fetchProducts is fulfilled");
+    // You can perform actions here after the API call is completed successfully
+  } else if (action.type === fetchProducts.rejected.type) {
+    // This block will intercept when fetchProducts is completed with an error
+    console.log("fetchProducts is rejected");
+    // You can perform actions here if the API call fails
+  }
+
+  return next(action);
+};
 
 export const store = configureStore({
   reducer: {
@@ -11,7 +53,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => {
     // Customize middleware here if needed
-    return getDefaultMiddleware();
+    return getDefaultMiddleware().concat(loggingMiddleware);
   },
 });
 
