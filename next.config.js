13d9696f8d25fcb9b58620d607c9f9c737e3a84e
prateek3065 <prateek.pixel@gmail.com>
2023-12-14 08:52:57 +0100

    installation of i18 and usage

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['i.dummyjson.com'], // Add the domain here
   },
+  i18n,
 };
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
-export default function Home() {
+import { appWithTranslation } from 'next-i18next';
+import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
+function Home() {
+  const { t } = useTranslation('common');
   // const task = useStore((state) => state.tasks);
 
   // useEffect(() => {
@@ -8,11 +11,22 @@ export default function Home() {
   // }, [dispatch]);
 
   return (
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', fontSize:'28px', fontFamily:'cursive'}}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: '28px',
+        fontFamily: 'cursive',
+      }}
+    >
       Root Page Please select App choice
-      <Link href={"counterApp"}>Counter App</Link>
-      <Link href={"dashboard"}>Dashboard</Link>
-      <Link href={"to-do-app"}>To-Do App</Link>
+      <Link href={'counterApp'}>Counter App</Link>
+      <Link href={'dashboard'}>Dashboard</Link>
+      <Link href={'to-do-app'}>To-Do App</Link>
+      <p>{t('hello')}</p>
     </div>
   );
 }
+
+export default appWithTranslation(Home);
